90231e4c196f85b20999c8914afb2cb87bf47d77
Stanislav Bezruchenko <Christopherhell23@gmail.com>
2021-08-09 21:28:06 +0300

    addClassCat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hWBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hWBean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cBean = (Cat) applicationContext.getBean("cat");
+        Cat cBean1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorldBeans = "+ (hWBean==hWBean1));
+        System.out.println("CatBeans = "+ (cBean==cBean1));
+
+
     }
 }
